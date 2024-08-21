e1293a3650c0ac5ec6e20be8cb49f47e326d0f7c
Adrian <adrian@seiboldsmuehle.de>
2024-08-21 04:12:09 +0200

    Added AccountService, DataBaseService, FrontEndInteractionService and TwoFactorAuthService classes
    Added these classes and implemented Login, GenerateAccountHash, HashPassword, CompareHashes, and GenerateSalt Methods in AccountService and added Methods, but did not implement that the Login method relies on.

---
+++ C#-Webserver/HttpsServer/server/DataBaseService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebHttpsServer.server
+{
+    public class UserNotFoundException : Exception
+    {
+
+    }
+
+    public static class DataBaseService
+    {
+        /// <summary>
+        /// Uses a primary key to get other user data out of a database.
+        /// </summary>
+        /// <param name="email">Uses the email as a primary key.</param>
+        /// <returns>Tuple containing (id, name, hashed password, salt, uses 2fa)</returns>
+        public static (int, string, string, byte[],bool) GetUserData(string email)
+        {
+            //byte[] salt = Encoding.ASCII.GetBytes(saltString);
+            throw new NotImplementedException();
+        }
+
+        public static string GetUserTwoFactorAuthSecret(string accountHash)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ C#-Webserver/HttpsServer/server/FrontEndInteractionService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebHttpsServer.server
+{
+    public static class FrontEndInteractionService
+    {
+        public static string RequestTwoFactorAuthCode()
+        {
+            // interact with frontend for 2FA code request
+            throw new NotImplementedException();
+        }
+    }
+}
