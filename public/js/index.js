9ba7d9a9139dc9b8fd5736bb9a65bc1a20afb341
WerbeName <werbename.btw@gmail.com>
2024-08-22 02:19:52 +0200

    login with google fixed
    fixed google logo and a new designs

--- public/js/index.js
+++ public/js/index.js
@@ -34,3 +34,41 @@ function openRegistration() {
     let registration = document.getElementById("registrationdialog");
     registration.setAttribute("open", "")
 }
+function openLogin() {
+    let registration = document.getElementById("registrationdialog");
+    registration.removeAttribute("open");
+    let login = document.getElementById("logindialog");
+    login.setAttribute("open", "")
+}
+
+var auth2;
+
+function initClient() {
+    gapi.load('auth2', function() {
+        auth2 = gapi.auth2.init({
+            client_id: '334094857641-ef4sau3ua1clm53gm6jcashbtkl3mpuf.apps.googleusercontent.com',
+            cookiepolicy: 'single_host_origin',
+        });
+    });
+}
+
+function signIn() {
+        auth2.signIn().then(function(googleUser) {
+            onSignIn(googleUser);
+        }).catch(function(error) {
+            console.error('Sign-in error', error);
+        });
+}
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId());
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    initClient();
+    document.getElementById('customGoogleSignIn').addEventListener('click', signIn);
+});
